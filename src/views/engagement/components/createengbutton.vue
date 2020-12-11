ece393c46398eb35a044c9a6587ef5b9417164ea
Mba Stephane <stephane.mba@snh.cm>
2020-12-11 15:32:17 +0100

    Initiating create engagement button

---
+++ src/views/engagement/components/createengbutton.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-button
+    :disabled="inactive"
+    type="primary"
+    icon="el-icon-edit"
+    @click="launchDialogForm"
+  >
+    Créer un engagement
+  </el-button>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Watch} from 'vue-property-decorator'
+
+@Component({
+  name: 'CreateEngButton',
+  components: {
+    
+  }
+})
+
+export default class CreateEngButton extends Vue {
+  @Prop() private inactive!: string
+}
+</script>
