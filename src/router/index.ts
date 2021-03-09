1da2854d9ba63a652695f986ef15acbc4e5256cd
jp_meka <jp_meka@hotmail.com>
2021-03-09 09:49:48 +0100

    towards adding maquette data

--- src/router/index.ts
+++ src/router/index.ts
@@ -233,7 +233,7 @@ export const asyncRoutes: RouteConfig[] = [
     meta: {
       title: 'etatsBudget',
       icon: 'dashboard',
-      teams: ['snh','snh_siege']
+      teams: ['snh','snh_yde']
     },
     children: [
       {
@@ -271,7 +271,7 @@ export const asyncRoutes: RouteConfig[] = [
         meta: {
           title: 'etatsFonctionnement',
           icon: 'tab',
-          teams: ['snh','snh_siege']
+          teams: ['snh','snh_yde']
         }
       },
       {
@@ -300,7 +300,7 @@ export const asyncRoutes: RouteConfig[] = [
         meta: {
           title: 'etatsMandat',
           icon: 'tab',
-          teams: ['snh'],
+          teams: ['snh_yde'],
         }
       }
     ]
@@ -314,7 +314,7 @@ export const asyncRoutes: RouteConfig[] = [
     meta: {
       title: 'rapports',
       icon: 'dashboard',
-      teams: ['cpsp','snhdouala']
+      teams: ['cpsp']
     },
     children : [
       {
