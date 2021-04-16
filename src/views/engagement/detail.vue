b9c10e5ac8ab9af8e8a929c8a6240a4e6ba70a33
Mba Stephane <stephane.mba@snh.cm>
2021-04-16 09:48:48 +0100

    updating package for installation

--- src/views/engagement/detail.vue
+++ src/views/engagement/detail.vue
@@ -3,6 +3,28 @@
     <el-row :gutter="30">
       <el-col :span="17" :offset="1">
         <h1>Détail {{ engagement.code }} </h1>
+        <header-buttons
+          :entity="engagement"
+          type="engagement"
+          :submit-disabled="submitUpdateDisabled"
+          :fallback-url="fallbackUrl"
+          :on-cancel="onCancel"
+          :update="updateSubmit"
+          :resend-update="resendUpdate"
+          :validerp-submit="validerpSubmit"
+          :validers-submit="validersSubmit"
+          :validerf-submit="validerfSubmit"
+          :restore="fbRestorePreeng"
+          :options-annuler-valider="optionsAnnulerValider"
+          :commentaire-submit="fbcommentaireSubmit"
+          :close="fbclosePreeng"
+          :send-back-submit="fbsendBackSubmit"
+          :cancel-valider-submit="fbcancelValiderSubmit"
+          :next-etat-action="launchImputer"
+          :next-etat-action-text="nextEtatActionText"
+          :is-next-etat-action="isNextEtatAction"
+          @footerload="loadHandler($event)"
+        />
       </el-col>
       <el-col :span="5">
         <!-- <h1>Historique</h1> -->
@@ -512,6 +534,7 @@ import {
 } from '@/api/engagements'
 import { getSoldeLigne } from '@/api/lignes'
 import { imputerEngagement } from '@/api/imputations'
+import HeaderButtons from '@/views/engagement/components/headerbuttons.vue'
 import FooterButtons from '@/views/engagement/components/footerbuttons.vue'
 import ImputationCard from '@/views/engagement/components/imputationcard.vue'
 import ApurementCard from '@/views/engagement/components/apurementcard.vue'
@@ -524,6 +547,7 @@ import { isNull } from 'lodash'
 @Component({
   name: 'DetailEngagement',
   components: {
+    HeaderButtons,
     FooterButtons
     , ImputationCard
     , ApurementCard
